a9a3afa453119126de0aaa3d371c2054f4c1c69f
osideskyler <osidesky@byu.edu>
2026-02-13 22:41:50 -0800

    sqlite

---
+++ mission6/Migrations/20260214062720_Initial.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace mission6.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    Director = table.Column<string>(type: "TEXT", nullable: false),
+                    Rating = table.Column<string>(type: "TEXT", nullable: false),
+                    Edited = table.Column<bool>(type: "INTEGER", nullable: true),
+                    LentTo = table.Column<string>(type: "TEXT", nullable: true),
+                    Notes = table.Column<string>(type: "TEXT", maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.MovieId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Applications",
+                columns: new[] { "MovieId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Sci-Fi", "Christopher Nolan", false, "", "Mind blowing", "PG-13", "Inception", 2010 },
+                    { 2, "Action", "Christopher Nolan", false, "", "Best Joker", "PG-13", "The Dark Knight", 2008 },
+                    { 3, "Comedy", "Jared Hess", true, "Joel", "Gosh!", "PG", "Napoleon Dynamite", 2004 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Applications");
+        }
+    }
+}
